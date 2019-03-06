4e3ce56869813232013d7f09770da0249349441f
Allen Luce <allen@github.con.com>
2019-03-06 14:31:10 -0800

    Multi-instance, limit number, and add tests
    You can now supply multiple instances on the command line and it'll
    actually work. It'll now output a table with instance type in it as
    well.
    
    The -n flag limits the number of line items to a specific count.
    
    A testing framework is added. The main module is modified a bit to
    support being run in test.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Change Log for awespottr
 
+## v0.4.0 [2019/03/06]
+
+- Get multi-instance search working
+- Add -n flag to limit output to top n cheapest options
+
 ## v0.3.0 [2018/05/26]
 
 - Fixing numeric comparison logic used to identify cheapest availability zone.
--- cli.js
+++ cli.js
@@ -6,22 +6,11 @@ var chalk = require('chalk');
 var moment = require('moment');
 var program = require('commander');
 
-var ec2Type = false;
+var ec2Type = [];
 var awsRegions = {};
 var zones = {};
 var lowPrice = false;
 var lowZone = false;
-
-program
-  .version('0.3.0')
-  .usage('[options] <EC2 instance types ...>')
-  .option('-r, --region <AWS region>', 'Limit to the given region')
-  .action(function(type) {
-    ec2Type = type;
-  })
-  .parse(process.argv);
-
-console.log('Checking spot prices for ' + ec2Type + ' instance type.');
 
 function getRegions() {
   return new Promise(function(resolve, reject) {
@@ -50,7 +39,7 @@ function getRegionSpots(regionName) {
   return new Promise(function(resolve, reject) {
     var ec2 = new AWS.EC2({apiVersion: '2016-04-01', region: regionName});
     var params = {
-      InstanceTypes: [ec2Type],
+      InstanceTypes: ec2Type,
       ProductDescriptions: ['Linux/UNIX', 'Linux/UNIX (Amazon VPC)'],
       StartTime: moment().subtract(4, 'hours').utc().toDate()
     };
@@ -65,10 +54,11 @@ function getRegionSpots(regionName) {
           if (sdata.SpotPrice && sdata.Timestamp && sdata.AvailabilityZone) {
             var sdate = moment(sdata.Timestamp);
             if (!zones.hasOwnProperty(sdata.AvailabilityZone) || zones[sdata.AvailabilityZone].lastDate < sdate) {
-              zones[sdata.AvailabilityZone] = {
+              zones[sdata.InstanceType + " " + sdata.AvailabilityZone] = {
                 lastDate: sdate,
                 lastPrice: sdata.SpotPrice,
-                zone: sdata.AvailabilityZone
+                zone: sdata.AvailabilityZone,
+                itype: sdata.InstanceType
               };
               if (!lowPrice || Number(sdata.SpotPrice) < lowPrice) {
                 lowPrice = Number(sdata.SpotPrice);
@@ -84,33 +74,68 @@ function getRegionSpots(regionName) {
 }
 
 function handleResults() {
-  console.log('\n' + _.padEnd('AWS Zone', 24) + ' ' + _.padEnd('Hourly Rate', 12));
-  console.log(_.pad('', 24, '-') + ' ' + _.pad('', 12, '-'));
+  console.log('\n' + _.padEnd('Instance Type', 16) + ' ' + _.padEnd('AWS Zone', 24) + ' ' + _.padEnd('Hourly Rate', 12));
+  console.log(_.pad('', 16, '-') + ' ' + _.pad('', 24, '-') + ' ' + _.pad('', 12, '-'));
   var sortedZones = _.values(zones);
   sortedZones = _.sortBy(sortedZones, function(val) { return Number(val.lastPrice); }, 'zone');
+  if (program.number > 0) {
+    sortedZones = sortedZones.slice(0, program.number)
+  }
   _.each(sortedZones, function(data) {
-    var msg = _.padEnd(data.zone, 24) + ' ' + _.padEnd('$' + data.lastPrice, 12);
-    if (data.zone === lowZone || data.lastPrice === lowPrice) {
+    var msg = _.padEnd(data.itype, 16) + ' ' + _.padEnd(data.zone, 24) + ' ' + _.padEnd('$' + data.lastPrice, 12);
+    if (data.zone === lowZone && Number(data.lastPrice) === Number(lowPrice)) {
       msg = chalk.green(msg);
     } else if (lowPrice * 1.1 >= data.lastPrice) {
       msg = chalk.yellow(msg);
     }
     console.log(msg);
   });
-  console.log(chalk.green('\nCheapest hourly rate for ' + ec2Type + ' is $' + lowPrice + ' in zone ' + lowZone));
+  console.log(chalk.green('\nCheapest hourly rate for [' + ec2Type.join(', ') + '] is $' + lowPrice + ' in zone ' + lowZone));
 }
 
-if (program.region) {
-  console.log('Limiting results to region ' + program.region);
-  getRegionSpots(program.region)
-    .then(handleResults)
-    .catch(function(err) {
-      console.log('error', err);
-    });
-} else {
-  getRegions(program.region)
-    .then(handleResults)
-    .catch(function(err) {
-      console.log('error', err);
-    });
+exports.standalone = function (cb) {
+  ec2Type = [];
+  awsRegions = {};
+  zones = {};
+  lowPrice = false;
+  lowZone = false;
+  program
+    .version('0.4.0')
+    .usage('[options] <EC2 instance types ...>')
+    .option('-r, --region <AWS region>', 'Limit to the given region')
+    .option('-n, --number <number to show>', 'Only show the top few cheapest spots')
+    .action(function(type, othertypes) {
+      ec2Type = []
+      ec2Type.push(type)
+      if (typeof othertypes === 'string' || othertypes instanceof String) {
+        ec2Type.push(othertypes)
+      }
+      if (Array.isArray(othertypes)) {
+        ec2Type.concat(othertypes)
+      }
+    })
+    .parse(process.argv);
+
+  console.log('Checking spot prices for [' + ec2Type.join(', ') + '] instance type(s).');
+
+  if (program.region) {
+    console.log('Limiting results to region ' + program.region);
+    getRegionSpots(program.region)
+      .then(handleResults)
+      .then(cb)
+      .catch(function(err) {
+        console.log('error', err);
+      });
+  } else {
+    getRegions(program.region)
+      .then(handleResults)
+      .then(cb)
+      .catch(function(err) {
+        console.log('error', err);
+      });
+  }
+}
+
+if (!module.parent) {
+  exports.standalone()
 }
